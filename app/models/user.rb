b6331f0e50df113fb9ad5761f8712f908efd21e4
Matt Denune <50151925+mattdenune@users.noreply.github.com>
2022-09-07 15:32:19 -0400

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   validates(:email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: true)
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,10 +14,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do 
     get signup_path
     assert_difference 'User.count', 1 do 
-      post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "12345678", password_confirmation: "12345678"}}
+      post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password"}}
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
