8570a7e9261dd09a877e61e483d50d3c27737e0d
Matthew Denune <mattdenune@gmail.com>
2023-01-30 13:23:40 -0500

    Small updates for funtionality

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -31,7 +31,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
-    # Simulate a user clicking logoir in a second window.
+    # Simulate a user clicking logout in a second window.
     delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,6 +18,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert_not flash.empty?
     assert is_logged_in?
   end
 end
